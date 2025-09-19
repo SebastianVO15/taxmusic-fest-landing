48c00d7b5681b7158d697d93a3d60ee080dafa94
SebastianVO15 <s.vargaso2@uniandes.edu.co>
2025-09-19 00:17:37 -0500

    Avances

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,9 @@
 import { Header } from "@/components/festival/Header";
 import { Hero } from "@/components/festival/Hero";
-import { About } from "@/components/festival/About";
-import { Zones } from "@/components/festival/Zones";
 import { Timeline } from "@/components/festival/Timeline";
 import { Activities } from "@/components/festival/Activities";
 import { LineUp } from "@/components/festival/LineUp";
-import { FoodTrucks } from "@/components/festival/FoodTrucks";
-import { Registration } from "@/components/festival/Registration";
-import { FAQ } from "@/components/festival/FAQ";
+import { PhotoUploader } from "@/components/PhotoUploader";
 import { Footer } from "@/components/festival/Footer";
 
 const Index = () => {
@@ -15,14 +11,18 @@ const Index = () => {
     <div className="min-h-screen">
       <Header />
       <Hero />
-      <About />
-      <Zones />
-      <Timeline />
-      <Activities />
-      <LineUp />
-      <FoodTrucks />
-      <Registration />
-      <FAQ />
+      <div id="lineup">
+        <LineUp />
+      </div>
+      <div id="timeline">
+        <Timeline />
+      </div>
+      <div id="activities">
+        <Activities />
+      </div>
+      <div id="photos">
+        <PhotoUploader />
+      </div>
       <Footer />
     </div>
   );
